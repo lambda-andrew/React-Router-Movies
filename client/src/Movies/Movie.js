3973e4c1ad368f68156d0e326451ae1ec859c468
Sydney Crumley <srsimps19@gmail.com>
2019-05-13 15:17:43 -0500

    added link to home button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,6 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
+    
     const id = 1;
     this.fetchMovie(id);
   }
